32ee052fb406bf38b957829b36a86951a608800c
Dhruthi Nooka <Dhruthi_Nooka@gap.com>
2022-01-07 16:47:59 +0530

    Added CORS

--- src/main/java/com/gap/hackathon/DataQuality/DQController/DataQualityController.java
+++ src/main/java/com/gap/hackathon/DataQuality/DQController/DataQualityController.java
@@ -10,17 +10,20 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 public class DataQualityController {
 
     @Autowired
     DataQualityService dataQualityService;
 
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
     @GetMapping("/fetchErrorCount")
     public List<ErrorCountDetails> getErrorCount() {
         return dataQualityService.getErrorCount();
     }
 
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
     @GetMapping("/fetchErrorData")
     public List<DataValidation1> fetchErrorData(@RequestParam String errorCode) {
         return dataQualityService.fetchErrorDataByErrorCode(errorCode);
